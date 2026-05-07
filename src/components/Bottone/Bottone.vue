1593e45dc471f35e5e8553aa91eb6a0e2386acb9
AntonioDS1 <159949547+AntonioDS1@users.noreply.github.com>
2026-05-07 15:18:03 +0200

    Update Bottone.vue

--- src/components/Bottone/Bottone.vue
+++ src/components/Bottone/Bottone.vue
@@ -21,10 +21,15 @@ const router = useRouter()
 const fullGrid = inject("fullGrid");
 const victory = inject("victory");
 
+const punteggioO = inject("punteggioO");
+const punteggioX = inject("punteggioX");
+
 function handleClick() {
     if (props.action === "restart") {
         router.replace('/griglia?r=' + Date.now());
     } else if (props.action === "back") {
+        punteggioO.value = 0;
+        punteggioX.value = 0;
         router.replace('/');
     }
 }
